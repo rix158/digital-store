914746e622367b96cdf0109b1f1bdf01da5884d6
rix158 <elrixo@gmail.com>
2024-01-20 19:49:47 -0500

    Add docker-compose file and ORM dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
